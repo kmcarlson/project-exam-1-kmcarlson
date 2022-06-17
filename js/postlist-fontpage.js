78f06216451307b5f00c8d7b7069e6da746a9b67
Kenneth Carlson <carlson90@hotmail.com>
2022-06-17 12:42:42 +0200

    .

---
+++ js/postlist-fontpage.js
@@ -0,0 +1,37 @@
+window.onload=generatePosts()
+
+async function getPosts(){
+    let posts = await fetch('https://newshop.kreatif.no/wp-json/wp/v2/posts?_embed&per_page=100')
+    let response = await posts.json()
+
+
+
+    return response
+}
+
+async function getPost(id) {
+    let post = await fetch(`https://newshop.kreatif.no/wp-json/wp/v2/posts/${id}?_embed&per_page=100`)
+    let response = await post.json()
+
+
+}
+
+async function generatePosts() {
+    let post = await getPosts()
+
+    console.log(post)
+    let num = 0;
+    let wrapper = document.getElementById('post-list' + num)
+    post.forEach(post => {
+        wrapper.innerHTML +=`
+            <a href=post.html?postId=${post.id} class="post">
+                <img src=${post._embedded['wp:featuredmedia'][0].source_url} class="featured-img">
+                <p>${post.title.rendered}</p>
+            </a>
+
+        
+        `
+        
+    });
+
+}
