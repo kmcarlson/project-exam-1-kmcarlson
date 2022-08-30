58be189bba5f877b8e399861b135b2908049ca44
Kenneth Carlson <carlson90@hotmail.com>
2022-08-30 11:29:56 +0200

    -

--- js/postlist.js
+++ js/postlist.js
@@ -22,15 +22,25 @@ async function generatePosts() {
     console.log(post)
 
     let wrapper = document.getElementById('post-list')
+    let count = 0;
     post.forEach(post => {
+        let postClass = 'postVisible';
+        if (count > 7) {
+                 postClass = 'postHidden';
+        }
+       
+        
         wrapper.innerHTML +=`
-            <a href=post.html?postId=${post.id} class="post">
+            <a href=post.html?postId=${post.id} class="post${postClass}">
                 <img src=${post._embedded['wp:featuredmedia'][0].source_url} class="featured-img circle">
                 <p class="headline">${post.title.rendered} </p>
             </a>
-
+            `
+      
+    count++;
         
-        `
+        
+   
         
     });
 
